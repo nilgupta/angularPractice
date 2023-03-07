d09dd31734b886f914804b86839b67b60d15f160
nilgupta <44253057+nilgupta@users.noreply.github.com>
2023-03-07 17:09:27 +0530

    string interpolation and property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+addNewServers =false;
+  constructor(  ) { 
+    setTimeout(() => {
+      this.addNewServers=true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
