4dc2a39a4c649a34ee8b39479808157abb7efac9
nilgupta <44253057+nilgupta@users.noreply.github.com>
2023-03-09 21:30:40 +0530

    two way binding using ngmodel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 addNewServers =false;
 serverCreationStatus = 'No server was created';
-serverName = '';
+serverName = 'TestServer';
   constructor(  ) { 
     setTimeout(() => {
       this.addNewServers=true;
@@ -18,7 +18,7 @@ serverName = '';
   ngOnInit(): void {
   }
 onchangeServerStatus(){
-  this.serverCreationStatus = 'server was created';
+  this.serverCreationStatus = 'server was created! name is' + this.serverName ;
 }
 onUpdateServerName(event :Event){
   this.serverName = (<HTMLInputElement>event.target).value;
